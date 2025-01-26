ba19d6049cd622a43264ea0b4000b193ca9ccf13
mariams58 <mariams58@yahoo.com>
2025-01-26 07:31:00 +0100

    Feat: Added the models for Result, user, Question and Category class

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from app import db
+
+#Writing The User class model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    results = db.relationship('Result', backref='user', lazy=True)
+
+# Writing the Category class model
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    questions = db.relationship('Question', backref='category', lazy=True)
+
+# Writing the Question class model
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    question_text = db.Column(db.String(200), nullable=False)
+    correct_answer = db.Column(db.String(100), nullable=False)
+    options = db.Column(db.JSON, nullable=False)
+
+# Witing the Result class model
+class Result(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    total_questions = db.Column(db.Integer, nullable=False)
